416e45e071feba4a9e435044947a68cb01e7dcfd
kelvinct <kelvinct@yahoo.com.hk>
2012-05-18 13:12:07 +0800

    2

--- GetONCC/Form1.Designer.vb
+++ GetONCC/Form1.Designer.vb
@@ -22,53 +22,66 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.components = New System.ComponentModel.Container()
+        Me.DataSet1 = New System.Data.DataSet()
+        Me.DgStatus = New System.Windows.Forms.DataGridView()
+        Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
         Me.Button1 = New System.Windows.Forms.Button()
-        Me.Button2 = New System.Windows.Forms.Button()
+        CType(Me.DataSet1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DgStatus, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
-        'TextBox1
+        'DataSet1
         '
-        Me.TextBox1.Location = New System.Drawing.Point(27, 24)
-        Me.TextBox1.Multiline = True
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(227, 180)
-        Me.TextBox1.TabIndex = 0
+        Me.DataSet1.DataSetName = "NewDataSet"
+        '
+        'DgStatus
+        '
+        Me.DgStatus.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DgStatus.Location = New System.Drawing.Point(12, 21)
+        Me.DgStatus.Name = "DgStatus"
+        Me.DgStatus.RowTemplate.Height = 24
+        Me.DgStatus.Size = New System.Drawing.Size(457, 403)
+        Me.DgStatus.TabIndex = 3
+        '
+        'StatusStrip1
+        '
+        Me.StatusStrip1.Location = New System.Drawing.Point(0, 459)
+        Me.StatusStrip1.Name = "StatusStrip1"
+        Me.StatusStrip1.Size = New System.Drawing.Size(482, 22)
+        Me.StatusStrip1.TabIndex = 4
+        Me.StatusStrip1.Text = "StatusStrip1"
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(47, 227)
+        Me.Button1.Location = New System.Drawing.Point(394, 430)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(75, 23)
-        Me.Button1.TabIndex = 1
+        Me.Button1.TabIndex = 5
         Me.Button1.Text = "Button1"
         Me.Button1.UseVisualStyleBackColor = True
-        '
-        'Button2
-        '
-        Me.Button2.Location = New System.Drawing.Point(157, 227)
-        Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(75, 23)
-        Me.Button2.TabIndex = 2
-        Me.Button2.Text = "Button2"
-        Me.Button2.UseVisualStyleBackColor = True
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(284, 262)
-        Me.Controls.Add(Me.Button2)
+        Me.ClientSize = New System.Drawing.Size(482, 481)
         Me.Controls.Add(Me.Button1)
-        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.StatusStrip1)
+        Me.Controls.Add(Me.DgStatus)
         Me.Name = "Form1"
         Me.Text = "Form1"
+        CType(Me.DataSet1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DgStatus, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
-    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents DataSet1 As System.Data.DataSet
+    Friend WithEvents DgStatus As System.Windows.Forms.DataGridView
+    Friend WithEvents StatusStrip1 As System.Windows.Forms.StatusStrip
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
     Friend WithEvents Button1 As System.Windows.Forms.Button
-    Friend WithEvents Button2 As System.Windows.Forms.Button
 
 End Class
--- GetONCC/Form1.vb
+++ GetONCC/Form1.vb
@@ -5,23 +5,36 @@ Imports System.Text
 Imports System.Text.RegularExpressions
 Imports System.Runtime.Serialization.Json
 Imports System.Web
+Imports System.Windows.Forms
 Public Class Form1
-
+    Dim dsStatus As New DataSet
+    Dim ServerCount As Integer
+    Dim dsXML As New DataSet
     Private Sub Form1_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
-        Button1.Enabled = True
-        Button2.Enabled = True
-
-        TextBox1.Text = EncodeText(GetOnCC("5"))
 
 
-
-
-
-
+        '  TextBox1.Text = EncodeText(GetOnCC("5")) + vbNewLine
+        ' Dim SLINE As String = "123"
+        GetStatus()
     End Sub
     Private Function EncodeText(ByVal sText As String) As String
         Return WebUtility.HtmlEncode(sText)
     End Function
+    Private Sub CallCurrentCellChanged()
+        AddHandler DgStatus.CurrentCellChanged, AddressOf Grid_CurCellChange
+    End Sub 'CallCurrentCellChanged
+
+    ' Raise the event when focus on DataGrid cell changes.
+    Private Sub Grid_CurCellChange(ByVal sender As Object, ByVal e As EventArgs)
+        ' String variable used to show message.
+        Dim myString As String = "CurrentCellChanged event raised, cell focus is at "
+        ' Get the co-ordinates of the focussed cell.
+        '   Dim myPoint As String = DgStatus.CurrentCell.ColumnNumber + "," + DgStatus.CurrentCell.RowNumber
+        ' Create the alert message.
+        '   myString = myString + "(" + myPoint + ")"
+        ' Show Co-ordinates when CurrentCellChanged event is raised.
+        MessageBox.Show(myString, "Current cell co-ordinates")
+    End Sub 'Grid_CurCellChange
 
     ' Dim js As New System.Web.Script.Serialization.JavaScriptSerializer
     'Dim rawdata = js.DeserializeObject(textAreaJson)
@@ -42,16 +55,88 @@ Public Class Form1
         Dim stocktype As String = "r"
         Dim sLine As String
         Dim nStart, nEnd As Integer
-        Dim stkprice As String
+        Dim StockName
+        Dim stkprice As String = 0
+
+        stocktype = "r"
         sLine = DownloadHTMLPage("http://money18.on.cc/js/" & stocktime & "/quote/" & Convert5(SCTYCode) & "_" & stocktype & ".js?t=1319009536902", "http://money18.on.cc/")
+        Dim SearchChar As String = "M18.r_"
+        If InStr(sLine, SearchChar) > 0 Then
 
-        Debug.Print(sLine)
 
-        GetOnCC = sLine
+            nStart = InStr(1, sLine, SearchChar) + Len(SearchChar)
+            Dim EndSearchChar As String = "="
+            nEnd = InStr(1, sLine, EndSearchChar)
+            StockName = Mid(sLine, nStart, nEnd - nStart)
 
+            SearchChar = "np: '"
+            nStart = InStr(nStart, sLine, SearchChar) + Len(SearchChar)
+            nEnd = InStr(nStart, sLine, "'")
+            stkprice = Mid(sLine, nStart, nEnd - nStart)
+
+
+            sLine = DownloadHTMLPage("http://money18.on.cc/js/daily/quote/" & Convert5(SCTYCode) & "_d.js?t=1319009536902", "http://money18.on.cc/")
+            SearchChar = "preCPrice:"""
+            nStart = InStr(1, sLine, SearchChar) + Len(SearchChar)
+            nEnd = InStr(nStart, sLine, """")
+            Dim StkpreCPrice = Mid(sLine, nStart, nEnd - nStart)
+            Debug.Print(StkpreCPrice)
+
+            SearchChar = "nameChi:"""
+            nStart = InStr(1, sLine, SearchChar) + Len(SearchChar)
+            nEnd = InStr(nStart, sLine, """")
+            Dim nameChi = (Mid(sLine, nStart, nEnd - nStart))
+            Dim Change
+            Change = ((stkprice - StkpreCPrice) / StkpreCPrice) * 100
+            Change = FormatNumber((stkprice - StkpreCPrice), 3)
+            GetOnCC = StockName + "," + nameChi + "," + stkprice + "," + Change
+
+
+            Debug.Print(GetOnCC)
+        Else
+
+            GetOnCC = "null,null,null,null"
+        End If
     End Function
 
+    Public Function DownloadDiableStatus() As String
+        Dim _PageContent As String = Nothing
+        Try
+            ' Open a connection
+            Dim _HttpWebRequest As System.Net.HttpWebRequest = CType(System.Net.HttpWebRequest.Create("http://us.battle.net/d3/en/status"), System.Net.HttpWebRequest)
 
+            _HttpWebRequest.ContentType = "application/x-www-form-urlencoded"
+            ' You can also specify additional header values like the user agent or the referer: (Optional)
+            _HttpWebRequest.UserAgent = "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)"
+            _HttpWebRequest.Referer = "http://us.battle.net/d3/en/status"
+
+            ' set timeout for 10 seconds (Optional)
+            _HttpWebRequest.Timeout = 10000
+
+            ' Request response:
+            Dim _WebResponse As System.Net.WebResponse = _HttpWebRequest.GetResponse()
+
+            ' Open data stream:
+            Dim _WebStream As System.IO.Stream = _WebResponse.GetResponseStream()
+            Dim encode As Encoding = System.Text.Encoding.GetEncoding("Big-5")
+            ' Create reader object:
+            Dim _StreamReader As New System.IO.StreamReader(_WebStream)
+            Dim readStream As New StreamReader(_WebStream, encode)
+            ' Read the entire stream content:
+            _PageContent = readStream.ReadToEnd()
+
+            ' Cleanup
+            _StreamReader.Close()
+            _WebStream.Close()
+            _WebResponse.Close()
+        Catch _Exception As Exception
+            ' Error
+            Console.WriteLine("Exception caught in process: {0}", _Exception.ToString())
+            Return Nothing
+        End Try
+
+        Return _PageContent
+    End Function
 
 
 
@@ -75,9 +160,9 @@ Public Class Form1
 
             ' Open data stream:
             Dim _WebStream As System.IO.Stream = _WebResponse.GetResponseStream()
-
+            Dim encode As Encoding = System.Text.Encoding.GetEncoding("Big5")
             ' Create reader object:
-            Dim _StreamReader As New System.IO.StreamReader(_WebStream)
+            Dim _StreamReader As New System.IO.StreamReader(_WebStream, encode)
 
             ' Read the entire stream content:
             _PageContent = _StreamReader.ReadToEnd()
@@ -95,15 +180,144 @@ Public Class Form1
         Return _PageContent
     End Function
 
-    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
-  TextBox1.Text = EncodeText(GetOnCC("5"))
+    '  Private Sub Button1_Click(sender As System.Object, e As System.EventArgs)
+    '     TextBox2.Text = ""
+    '      TextBox1.Text = ""
+
+    ' Dim SLINE As String = "123"
+    ' End Sub
+
+    ' Private Sub TextBox2_TextChanged(sender As System.Object, e As System.EventArgs)
+
+
+    '  End Sub
+
+    '  Private Sub TextBox2KeyPress(sender As System.Object, e As System.Windows.Forms.KeyPressEventArgs)
+    '     If Asc(e.KeyChar) = 32 Or Asc(e.KeyChar) = 13 Then
+    '         If Len(TextBox2.Text) > 0 Then
+    '             TextBox1.AppendText(EncodeText(GetOnCC(TextBox2.Text)) + vbNewLine)
+    '         End If
+
+    '     End If
+
+    ' End Sub
+
+
+
+
+
+
+    Sub GetStatus()
+        Dim dt As DataTable
+        Dim ModuleColumn As DataColumn
+        Dim DescriptionCoulumn As DataColumn
+        Dim nameCoulumn As DataColumn
+        Dim CheckCoulumn As DataColumn
+        Dim dr As DataRow
+
+        dt = New DataTable()
+        ModuleColumn = New DataColumn("Stock Code", Type.GetType("System.String"))
+        DescriptionCoulumn = New DataColumn("stock Name", Type.GetType("System.String"))
+        nameCoulumn = New DataColumn("Price", Type.GetType("System.String"))
+        CheckCoulumn = New DataColumn("Change", Type.GetType("System.String"))
+
+        dt.Columns.Add(ModuleColumn)
+        dt.Columns.Add(DescriptionCoulumn)
+        dt.Columns.Add(nameCoulumn)
+        dt.Columns.Add(CheckCoulumn)
+        dsStatus.Tables.Add(dt)
+
+        DgStatus.DataSource = dsStatus.Tables(0)
+        dsStatus.Tables(0).Clear()
+        DgStatus.Columns("Stock Code").Width = 100
+        DgStatus.Columns("Stock Name").Width = 100
+        DgStatus.Columns("Price").Width = 100
+        DgStatus.Columns("Change").Width = 100
+        '   DgStatus.Columns("Check").HeaderText = "Checking"
+
+
+        dsStatus.Tables(0).Rows.Add(dr)
+    End Sub
+
+    Private Sub DgStatus_CellContentClick(sender As System.Object, e As System.Windows.Forms.DataGridViewCellEventArgs) Handles DgStatus.CellContentClick
+        Dim i, j As Integer
+        i = DgStatus.CurrentRow.Index
+        Debug.Print(DgStatus.Item(0, i).Value)
+        Debug.Print(DgStatus.Item(1, i).Value)
+        Debug.Print(DgStatus.Item(2, i).Value)
+        'Debug.Print(tem(3, i).Value
+    End Sub
+
+    Private Sub GridKeyPress(sender As System.Object, e As System.Windows.Forms.KeyPressEventArgs) Handles DgStatus.KeyPress
 
 
     End Sub
 
-    Private Sub Button2_Click(sender As System.Object, e As System.EventArgs) Handles Button2.Click
-        TextBox1.Text = EncodeText(GetOnCC("358"))
+    Private Sub DgStatus__RowEnter(sender As System.Object, e As System.Windows.Forms.DataGridViewCellEventArgs) Handles DgStatus.RowEnter
+        Dim i As Integer
+        For i = 0 To DgStatus.Rows(e.RowIndex).Cells.Count - 1
+            DgStatus(i, e.RowIndex).Style _
+                .BackColor = Color.Yellow
+        Next i
+    End Sub
 
+    Private Sub DgStatus_RowLeave(sender As System.Object, e As System.Windows.Forms.DataGridViewCellEventArgs) Handles DgStatus.RowLeave
+        Dim i As Integer
+        For i = 0 To DgStatus.Rows(e.RowIndex).Cells.Count - 1
+            DgStatus(i, e.RowIndex).Style _
+                .BackColor = Color.Empty
+        Next i
+    End Sub
+
+    Private Sub DgStatus_CellValidated(sender As System.Object, e As System.Windows.Forms.DataGridViewCellEventArgs) Handles DgStatus.CellValidated
+        Dim headerText As String = DgStatus.Columns(e.ColumnIndex).HeaderText
+
+        ' Abort validation if cell is not in the CompanyName column.
+        If Not headerText.Equals("Stock Code") Then Return
+        Dim a As Array
+        Dim StockCode = (DgStatus.Item(e.ColumnIndex, e.RowIndex).Value.ToString())
+        If IsNumeric(StockCode) Then
+            Debug.Print(e.ColumnIndex)
+            Debug.Print(e.RowIndex)
+
+
+            a = EncodeText(GetOnCC(StockCode)).Split(",")
+            If a(0) = "null" Then
+
+
+                Return
+            End If
+            Debug.Print(a(0))
+            DgStatus.Item(e.ColumnIndex + 1, e.RowIndex).Value = (a(1))
+            DgStatus.Item(e.ColumnIndex + 2, e.RowIndex).Value = (a(2))
+            DgStatus.Item(e.ColumnIndex + 3, e.RowIndex).Value = (a(3))
+        Else
+            Dim i As Integer = (DgStatus.Rows.Count - 1)
+            Do While (i >= 0)
+                i = (i - 1)
+                For Each dr As DataGridViewRow In DgStatus.Rows
+                    If dr.Cells("Price").Value Is Nothing Then
+                        DgStatus.Rows.Remove(dr)
+                    End If
+                Next
+            Loop
+         
+        End If
+
+
+
+    End Sub
+
+    Private Sub DgStatus_CellValidating(sender As System.Object, e As System.Windows.Forms.DataGridViewCellValidatingEventArgs) Handles DgStatus.CellValidating
+
+    End Sub
+
+    Private Sub DgStatus_RowsRemoved(sender As System.Object, e As System.Windows.Forms.DataGridViewRowsRemovedEventArgs) Handles DgStatus.RowsRemoved
+
+    End Sub
+
+    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
+        DgStatus.Rows.RemoveAt(0)
     End Sub
 End Class
 
